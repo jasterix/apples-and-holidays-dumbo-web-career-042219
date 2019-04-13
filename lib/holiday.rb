00db4395a89a939e60529b95af861e438dd15be6
Jasterix C <loune.calixte@gmail.com>
2019-04-13 03:41:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each {|season, holiday|
-    puts season.join(": ")
+    puts season <<": ")
 #    season = season.join(": ")
   }
     holiday.each {|holiday, supplies|
