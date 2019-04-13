23a4d7febd625d9408d7c5513f1624346afcbdc9
Jasterix C <loune.calixte@gmail.com>
2019-04-13 04:43:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -103,6 +103,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday.collect {|holiday, lists|
       if lists.include?"BBQ"
         if arr.include?(holiday)
+        else
         holiday
         arr.unshift(holiday)
       end
