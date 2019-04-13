557c3ce5f3a4887f5c206a13e6b20131709b39d6
Jasterix C <loune.calixte@gmail.com>
2019-04-13 03:46:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,10 +69,9 @@ def all_supplies_in_holidays(holiday_hash)
   }
     holiday.each {|holiday, supplies|
       supplies = supplies.join(", ")
-  }
-  holiday = holiday.to_s.split("_").map {|i| i.capitalize}.join(" ") << ":"
-  puts "  " + holiday + " " + supplies
-
+    holiday = holiday.to_s.split("_").map {|i| i.capitalize}.join(" ") << ":"
+    puts "  " + holiday + " " + supplies
+}
 end
 
 def all_holidays_with_bbq(holiday_hash)
