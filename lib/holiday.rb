439d67464c503cc27d7666711a1b2d0dc59459e0
Jasterix C <loune.calixte@gmail.com>
2019-04-13 04:17:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -99,7 +99,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   arr = []
-  holiday_hash.each {|season, holiday|
+  holiday_hash.collect {|season, holiday|
     holiday.collect {|holiday, lists|
       if lists.include?"BBQ"
         holiday
