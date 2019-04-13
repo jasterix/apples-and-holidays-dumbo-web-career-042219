2128157862fd0b9efe3cc19c69256761225ef29c
Jasterix C <loune.calixte@gmail.com>
2019-04-13 03:32:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,8 +48,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  arr = holiday_hash
-  [:winter].flatten
+  arr = holiday_hash[:winter].map{||
+  .flatten
 
 end
 
